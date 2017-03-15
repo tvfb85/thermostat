190b9ab1835ffb9e36d6b5b376f7ee0adeb56a5b
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-15 12:33:03 +0000

    Implement minimum temperature functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,14 +11,17 @@ describe('Thermostat', function() {
   });
 
   it('increases the temperature by 1', function() {
-    thermostat.increaseTemp(1)
+    thermostat.increaseTemp(1);
     expect(thermostat.temp).toEqual(21);
   });
 
   it('decreases the temperature by 1', function() {
-    thermostat.decreaseTemp(1)
+    thermostat.decreaseTemp(1);
     expect(thermostat.temp).toEqual(19);
   });
 
-
+  it('temperature cannot be set below 10 degrees', function() {
+    thermostat.decreaseTemp(11);
+    expect(thermostat.temp).toEqual(10);
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,13 +1,14 @@
 function Thermostat() {
-  var DEFAULT_TEMP = 20
+  var DEFAULT_TEMP = 20;
+  var MINIMUM_TEMP = 10;
   this.temp = DEFAULT_TEMP;
+  this.minimumTemp = MINIMUM_TEMP;
 }
-
 
 Thermostat.prototype.increaseTemp = function(degrees) {
   this.temp += degrees;
-}
+};
 
 Thermostat.prototype.decreaseTemp = function(degrees) {
-  this.temp -= degrees;
-}
+  this.temp - degrees < this.minimumTemp ? this.temp = this.minimumTemp : this.temp -= degrees;
+};
