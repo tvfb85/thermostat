f79a2224fb49a10094ab141efe643af42b8d5757
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-15 12:17:30 +0000

    Add thermostat temp increase/decrease

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,24 @@
 describe('Thermostat', function() {
 
-  var thermostat = new Thermostat();
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   it('starts at 20 degrees', function() {
     expect(thermostat.temp).toEqual(20);
   });
+
+  it('increases the temperature by 1', function() {
+    thermostat.increaseTemp(1)
+    expect(thermostat.temp).toEqual(21);
+  });
+
+  it('decreases the temperature by 1', function() {
+    thermostat.decreaseTemp(1)
+    expect(thermostat.temp).toEqual(19);
+  });
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,3 +2,12 @@ function Thermostat() {
   var DEFAULT_TEMP = 20
   this.temp = DEFAULT_TEMP;
 }
+
+
+Thermostat.prototype.increaseTemp = function(degrees) {
+  this.temp += degrees;
+}
+
+Thermostat.prototype.decreaseTemp = function(degrees) {
+  this.temp -= degrees;
+}
